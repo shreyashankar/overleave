4ddf670f293ec5ec57ea6f44ed7dc1bb344e7830
Shreya Shankar <ss.shankar505@gmail.com>
2021-09-30 15:35:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Overleave
+
+This project is a Chrome extension that, when in an Overleaf project, opens the compiled pdf in a new tab. I built this because I prefer the local TeX experience where I can typeset in one window and leave the compiled pdf in another window; however, Overleaf doesn't have this functionality.
+
+Features:
+
+* Supports private Overleaf projects
+* Automatically rerenders pdf when the Overleaf project recompiles
+
+## Setup
+
+You can download the latest release from the Chrome extension store. To run in developer mode, you can:
+
+1. Clone this repository
+2. Navigate to [chrome://extensions/](chrome://extensions/)
+3. Enable the "Developer mode" switch in the top right
+4. Click "Load Unpacked" in the left hand side
+5. Upload the project
+
+## Contributing
+
+It's pretty hacky Javascript right now, so if you're interested in making it better, feel free to make a PR!
---
+++ content.js
@@ -0,0 +1,57 @@
+var oldURL = "";
+var newURL = "";
+var w = window.open(oldURL, "_blank");
+
+function updateWindow(elem) {
+    newURL = elem.href.slice(0, -19);
+    if (newURL !== oldURL) {
+        oldURL = newURL;
+        w.location.replace(newURL);
+    }
+}
+
+function waitForBuild() {
+    return new Promise(function (resolve, reject) {
+        var counter = 0;
+
+        var checkExist = setInterval(function () {
+            counter += 1;
+            if (counter > 100) {
+                clearInterval(checkExist);
+            }
+
+            const matches = document.body.getElementsByTagName("a");
+
+            for (const match of matches) {
+                let url = JSON.stringify(match.href);
+                if (url.includes("build") && url.includes("pdf") && url.includes("download")) {
+                    clearInterval(checkExist);
+                    updateWindow(match);
+                    resolve(match);
+                    break;
+                }
+            }
+
+        }, 500);
+    });
+}
+
+
+waitForBuild().then((elem) => {
+
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.type == "attributes") {
+                updateWindow(elem);
+            }
+        });
+    });
+
+    observer.observe(elem, {
+        attributes: true
+    });
+
+}).catch(err => {
+    //TODO(shreyashankar): do something better here
+    console.log(err);
+});
